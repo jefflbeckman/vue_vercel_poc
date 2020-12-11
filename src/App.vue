576bed7d98dce75506f542a3bc4889b55760343f
Jeffrey Beckman <jefflbeckman@gmail.com>
2020-12-11 05:04:18 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Vercel proof of concept."/>
   </div>
 </template>
 
